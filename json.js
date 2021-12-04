12c6e0a82c15e65ffe53dbec1f935c9f671ce9e6
Tosin <oladejit3@gmail.com>
2021-12-04 18:17:55 +0100

    added changes

--- json.js
+++ json.js
@@ -12,13 +12,15 @@ const fs = require("fs");
 
 const dataBuffer = fs.readFileSync("1-json.json");
 const dataJSON = dataBuffer.toString();
-const data = JSON.parse(dataJSON);
-console.log(data.name);
+const user = JSON.parse(dataJSON);
+// console.log(data.name);
 
-const user = "Tosin";
-const age = 54;
-
-const userJson = Json.stringify(user);
+// const user = "Tosin";
+// const age = 54;
+user.name = "coolboy";
+user.age = "12";
+const userJson = JSON.stringify(user);
+console.log(userJson);
 fs.writeFileSync("1-json.json", userJson);
 // const parseData = JSON.parse(bookJson);
 // console.log(parseData.author);
