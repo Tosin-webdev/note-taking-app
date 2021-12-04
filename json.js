4d76c49bd57641075906ac66d72d525a25498fb6
Tosin <oladejit3@gmail.com>
2021-12-04 18:01:21 +0100

    update file

--- json.js
+++ json.js
@@ -1,12 +1,24 @@
 const fs = require("fs");
 
-const book = {
-  title: "E go be",
-  author: "Tosin",
-};
+// const book = {
+//   title: "E go be",
+//   author: "Tosin",
+// };
 
-const bookJson = JSON.stringify(book);
-console.log(bookJson);
+// const dataBuffer = fs.readFileSync("1-json.json");
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+// console.log(data.name);
 
-const parseData = JSON.parse(bookJson);
-console.log(parseData.author);
+const dataBuffer = fs.readFileSync("1-json.json");
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data.name);
+
+const user = "Tosin";
+const age = 54;
+
+const userJson = Json.stringify(user);
+fs.writeFileSync("1-json.json", userJson);
+// const parseData = JSON.parse(bookJson);
+// console.log(parseData.author);
